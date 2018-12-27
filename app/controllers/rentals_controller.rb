8a4a3872e8a69f4d98e61930187bf11d9cfaace2
alexg622 <alex.gm62288@gmail.com>
2018-12-27 13:56:53 -0800

    Handle rental update errors

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -33,25 +33,29 @@ class RentalsController < ApplicationController
       if @rental.update_attributes(cost: rental_params[:cost], rental_name: rental_params[:rental_name], description: rental_params[:description], additional_info: rental_params[:additional_info], contact_number: rental_params[:contact_number], contact_email: rental_params[:contact_email], addressLN2: rental_params[:addressLN2], addressLN1: rental_params[:addressLN1], state: rental_params[:state], city: rental_params[:city], zipcode: rental_params[:zipcode], start_date: @rental.start_date, end_date: @rental.end_date) && @rental.image.attached?
         redirect_to user_path(current_user)
       else
-        redirect_to root_path
+        flash.now[:error] = @rental.errors.full_messages.join(", ") + (@rental.errors.full_messages.length == 0 ? "please attach a picture" : ", please attach a picture")
+        render "edit"
       end
     elsif rental_params[:end_date] == ""
       if @rental.update_attributes(cost: rental_params[:cost], rental_name: rental_params[:rental_name], description: rental_params[:description], additional_info: rental_params[:additional_info], contact_number: rental_params[:contact_number], contact_email: rental_params[:contact_email], addressLN2: rental_params[:addressLN2], addressLN1: rental_params[:addressLN1], state: rental_params[:state], city: rental_params[:city], zipcode: rental_params[:zipcode], start_date: rental_params[:start_date], end_date: @rental.end_date) && @rental.image.attached?
         redirect_to user_path(current_user)
       else
-        redirect_to root_path
+        flash.now[:error] = @rental.errors.full_messages.join(", ") + (@rental.errors.full_messages.length == 0 ? "please attach a picture" : ", please attach a picture")
+        render "edit"
       end
     elsif rental_params[:start_date] == ""
       if @rental.update_attributes(cost: rental_params[:cost], rental_name: rental_params[:rental_name], description: rental_params[:description], additional_info: rental_params[:additional_info], contact_number: rental_params[:contact_number], contact_email: rental_params[:contact_email], addressLN2: rental_params[:addressLN2], addressLN1: rental_params[:addressLN1], state: rental_params[:state], city: rental_params[:city], zipcode: rental_params[:zipcode], start_date: @rental.start_date, end_date: rental_params[:end_date]) && @rental.image.attached?
         redirect_to user_path(current_user)
       else
-        redirect_to root_path
+        flash.now[:error] = @rental.errors.full_messages.join(", ") + (@rental.errors.full_messages.length == 0 ? "please attach a picture" : ", please attach a picture")
+        render "edit"
       end
     else
       if @rental.update_attributes(rental_params) && @rental.image.attached?
         redirect_to user_path(current_user)
       else
-        redirect_to root_path
+        flash.now[:error] = @rental.errors.full_messages.join(", ") + (@rental.errors.full_messages.length == 0 ? "please attach a picture" : ", please attach a picture")
+        render "edit"
       end
     end
   end
