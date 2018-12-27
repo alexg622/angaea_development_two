7aedfb62e5b5db5a5784897ac00519e01e447632
alexg622 <alex.gm62288@gmail.com>
2018-12-27 00:34:43 -0800

    Make it so you can't buy a ticket if they are all purchased

--- app/controllers/activity_tickets_controller.rb
+++ app/controllers/activity_tickets_controller.rb
@@ -8,26 +8,25 @@ class ActivityTicketsController < ApplicationController
     # Amount in cents
     @activity = Activity.find(params[:activity_id])
     @amount = @activity.cost.to_i*100
+      customer = Stripe::Customer.create(
+        :email => params[:stripeEmail],
+        :source  => params[:stripeToken]
+      )
 
-    customer = Stripe::Customer.create(
-      :email => params[:stripeEmail],
-      :source  => params[:stripeToken]
-    )
-
-    charge = Stripe::Charge.create(
-      :customer    => customer.id,
-      :amount      => @amount,
-      :description => 'Rails Stripe customer',
-      :currency    => 'usd'
-    )
-    if charge.status == "succeeded"
-      @activity_ticket = ActivityTicket.new(user_id: current_user.id, activity_id: @activity.id)
-      if @activity_ticket.save
-        return redirect_to user_path(current_user)
+      charge = Stripe::Charge.create(
+        :customer    => customer.id,
+        :amount      => @amount,
+        :description => 'Rails Stripe customer',
+        :currency    => 'usd'
+      )
+      if charge.status == "succeeded"
+        @activity_ticket = ActivityTicket.new(user_id: current_user.id, activity_id: @activity.id)
+        if @activity_ticket.save
+          return redirect_to user_path(current_user)
+        end
       end
+    rescue Stripe::CardError => e
+      flash[:error] = e.message
+      return redirect_to :root
     end
-  rescue Stripe::CardError => e
-    flash[:error] = e.message
-    return redirect_to :root
-  end
 end
